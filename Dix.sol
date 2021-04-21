3c6bb3229d743d8b33f5e217ad81f788cc52b54c
Dix-Development <dix.devteam@gmail.com>
2021-04-21 16:33:21 -0600

    Fixing the approval requirement for  transferFrom

--- Dix.sol
+++ Dix.sol
@@ -242,7 +242,10 @@ contract ERC20 is Context, IERC20 {
      */
     function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
         _transfer(sender, recipient, amount);
-        _approve(sender, _msgSender(), amount);
+
+        uint256 currentAllowance = _allowances[sender][_msgSender()];
+        require(currentAllowance >= amount, "ERC20: transfer amount exceeds allowance");
+        _approve(sender, _msgSender(), currentAllowance - amount);
 
         return true;
     }
